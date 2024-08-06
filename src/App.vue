9d596b7458500cf062d10cbe7fa9eeea524b58df
Mangassouba <hamallahmanga@gmail.com>
2024-08-06 11:48:48 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <HelloWorldCompnents
+    @update-name="name => display(name)"
+    />
+
+    <span>Bonjours, j'ai m'appelle {{ name }} et j'ai {{ age }} ans</span>
+   
+</template>
+
+
+
+<script setup>
+    import HelloWorldCompnents from './components/HelloWorldCompnents.vue';
+
+    import { ref } from 'vue';
+
+    const name = ref('NordCoders')
+    const age = ref(29)
+
+
+    const display = (name) => {
+        console.log('App.vue : ' + name)
+    }
+
+</script>
+
+
+
+<style scoped>
+ .bold {
+    font-weight: bolder;
+ }
+
+ .test-red{
+    color: red;
+ }
+
+ .test-green{
+    color: green;
+ }
+ span{
+    display: block;
+    color: green;
+    margin-top: 10px;
+ }
+
+</style>
+ 
---
+++ src/components/HelloWorldCompnents.vue
@@ -0,0 +1,35 @@
+<template>
+    <h1>Hello World</h1>
+
+   
+    {{ name }}
+   
+    <input type="text" v-model="name" @input="handleUpdate">
+    
+  
+</template>
+
+    
+
+<script setup>
+
+import { ref } from 'vue';
+
+const emit = defineEmits(['updateName']);
+
+const handleUpdate = (event) => {
+    // console.log(event.target.value);
+
+    emit('updateName',event.target.value)
+}
+
+const name = ref('');
+</script>
+
+
+
+
+
+
+<style scoped>
+</style>
